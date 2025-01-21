97798e11cbc158cae4bb2a5277ca013397f51d08
rahull0328 <rmmehta2004@gmail.com>
2025-01-21 19:31:24 +0530

    Started working on messages module

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -10,8 +11,10 @@ dotenv.config();
 app.use(express.json());
 //middleware for cookie authentication
 app.use(cookieParser());
-app.use("/api/auth", authRoutes);
 
+//api routes
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
