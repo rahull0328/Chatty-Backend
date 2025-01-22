18c716ecf22ca8947f59f113f0ff4582ca10c7b0
rahull0328 <rmmehta2004@gmail.com>
2025-01-22 19:12:26 +0530

    completed message controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -15,5 +16,20 @@ export const getUsersForSidebar = async (req, res) => {
 };
 
 export const getMessages = async (req, res) => {
-    
-}
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages: ", error.message);
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
